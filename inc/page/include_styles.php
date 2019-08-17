3b72f21a8782a06c91eabce0be2864f678991cda
Emrullah TAŞTAN <emrullah.tastan@hotmail.com>
2019-08-18 02:49:09 +0300

    Branch test

--- inc/page/include_styles.php
+++ inc/page/include_styles.php
@@ -3,3 +3,5 @@
 wp_enqueue_style('favorite_style', '/wp-content/plugins/plugin-emrullah/inc/css/favorite.css');
 wp_enqueue_style('bootstrap_style', '//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
 
+
+
